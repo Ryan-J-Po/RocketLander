327b90a8ca9fb689ee3f60e1cdead879a0befb78
Ryan Powell <s228032@students.aie.edu>
2023-04-12 06:58:48 -0500

    Tweaked LandingBehaviour/ Still not working as desired..still compiles.

--- Assets/Scripts/LandingBehaviour.cs
+++ Assets/Scripts/LandingBehaviour.cs
@@ -17,15 +17,21 @@ public class LandingBehaviour : MonoBehaviour
 
     private float _score = 1;
 
+    public Rigidbody _rigidBody;
+
 
     private void Start()
     {
-        
+        //_rigidBody = GetComponent<Rigidbody>();
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("A collision has occurred.");
+
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.isKinematic = false;
 
         if (!collision.gameObject.CompareTag("LandingPad") || !collision.gameObject.CompareTag("Ground"))
         {
@@ -42,28 +48,28 @@ public class LandingBehaviour : MonoBehaviour
         {
             if (_greenPad)
             {
+                Debug.Log("Green Landing Pad Found."); 
                 _score *= 2;      
             }
 
             if (_yellowPad)
             {
+                Debug.Log("Yellow Landing Pad Found.");
                 _score *= 3;
             }
 
             if (_redPad)
             {
+                Debug.Log("Red Landing Pad Found.");
                 _score *= 5;
             }
         }
 
         if (collision.gameObject.CompareTag("Ground"))
         {
+            Debug.Log("Ground Found.");
             _score *= 1;
         }
 
-        //if (collision)
-        //{
-        //    gameObject.IsKinematic == true;
-        //}
     }
 }
